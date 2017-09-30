1c358f67fa9f35f5ddb0caa99630e2383f623657
adriandarian <adarian@ucmerced.edu>
2017-09-30 05:08:19 -0700

    Add files via upload

---
+++ cam.js
@@ -0,0 +1,25 @@
+function readURL(input) {
+  if (input.files && input.files[0]) {
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+      $('#blah').attr('src', e.target.result);
+    }
+
+    reader.readAsDataURL(input.files[0]);
+  }
+}
+
+$("#imgInp").change(function(){
+  readURL(this);
+});
+
+/*function show_image(src, width, height, alt) {
+  var img = document.createElement("img");
+  img.src = src;
+  img.width = width;
+  img.height = height;
+  img.alt = alt;
+
+  document.body.appendChild(img);
+}*/
---
+++ process.php
@@ -0,0 +1,15 @@
+<?php
+  session_start();
+
+  ########################
+  $name = $_POST['name'];
+  ########################
+
+if ($name == $name) {
+  header("Location: index.html?")
+}
+
+  $_SESSION["email"] = $email;
+
+  header('Location: ./page-2.html')
+ ?>
