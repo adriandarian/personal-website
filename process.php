d27bfa9f8b53604c24970dff368de7b29021eb3e
adriandarian <adarian@ucmerced.edu>
2017-09-30 05:58:58 -0700

    Add files via upload

--- process.php
+++ process.php
@@ -5,9 +5,9 @@
   $name = $_POST['name'];
   ########################
 
-if ($name == $name) {
-  header("Location: index.html?")
-}
+  if ($name == $name) {
+    header("Location: index.html?id")
+  }
 
   $_SESSION["email"] = $email;
 
